23559450656a274162ea913ed8d52fcd5a2c4fa7
Riyaz Maderbocus <madsonray0@gmail.com>
2026-07-27 10:03:37 +0100

    start bioreactor flow rate changes

--- src/components/Forms/BioreactorUpdateForm.jsx
+++ src/components/Forms/BioreactorUpdateForm.jsx
@@ -7,7 +7,7 @@ const BioreactorUpdateForm = ({unitOperation, closeModal}) => {
 
     const {data, title} = unitOperation
     const {vesselVolume, vvd, flowRatemlmin, flowRatelh, titremgml, titremgmin} = data
-        const [updateBioreactorFormData, setUpdateBioreactorFormData] = useState({
+        const [bioreactorFormData, setBioreactorFormData] = useState({
         title,
         vesselVolume,
         vvd,
@@ -17,42 +17,109 @@ const BioreactorUpdateForm = ({unitOperation, closeModal}) => {
         titremgmin
 
     })
+
+    const handleFormChange = (e) => {
+        setBioreactorFormData({
+            ...bioreactorFormData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleFormChangeNumber = (e) => {
+        setBioreactorFormData({
+            ...bioreactorFormData,
+            [e.target.name]: parseFloat(e.target.value)
+        })
+    }
+
+    // const oldValueChecker = (nameCheck, checkAgainst, trueValue, fasleValue)=> {
+    //     if (nameCheck === checkAgainst){
+
+    //     }
+    // }
+
+    const handleFlowRateChange = (e) => {
+        let newVesselVolume = 0
+        // Update new vessel volumes
+        if (e.target.name === "vesselVolume"){
+            newVesselVolume = parseFloat(e.target.value)
+        } else {
+            newVesselVolume = vesselVolume
+        }
+
+        // Updated VVD
+        let newVvd = 0
+        if (e.target.name === "vvd"){
+            newVvd = parseFloat(e.target.value)
+        } else {
+            newVvd = vvd
+        }
+
+        const newFlowRateLh = parseFloat((vvd * (newVesselVolume /24)).toFixed(2))
+        const newFlowRatemlmin = parseFloat((newFlowRateLh * 1000 / 60).toFixed(2))
+        setBioreactorFormData({
+            ...bioreactorFormData,
+            [e.target.name]: parseFloat(e.target.value),
+            flowRatemlmin: newFlowRatemlmin,
+            flowRatelh: newFlowRateLh
+        })
+
+    }
+
     return ( 
         <form className="form-container">
-            <FormTextInput label="Title" value={title}/>
+            <FormTextInput label="Title" name="title" value={bioreactorFormData.title}
+            onChange={handleFormChange}/>
 
-            <p className="form-separator">Flow rates</p>
+            {/* <p className="form-separator">Vessel Volume and VVD</p> */}
 
             <div className="form-input-cols">
-                <div className="form-input-column">
-                    <FormNumberInputSmall label="VVD" />
-                </div>
+
                 <div className="form-input-column-center">
+                    <FormNumberInputSmall label="Vessel Volume L" name="vesselVolume" 
+                    value={bioreactorFormData.vesselVolume} 
+                    onChange={handleFlowRateChange}/>
+                </div>
+  
+                  <div className="form-input-column-center">
                     <p className="form-input-column-text-label">ml/min</p>
-                    <p className="form-input-column-text-label">{flowRatemlmin}</p>
+                    <p className="form-input-column-text-label">{bioreactorFormData.flowRatemlmin}</p>
                     
                 </div>
+
+            </div>
+
+            {/* <p className="form-separator">Flow rates</p> */}
+
+            <div className="form-input-cols">
+  
+               <div className="form-input-column-center">
+                    <FormNumberInputSmall label="VVD" name="vvd" 
+                    value={bioreactorFormData.vvd}
+                    onChange={handleFlowRateChange}/>
+                </div>
+
                 <div className="form-input-column-center">
                     <p className="form-input-column-text-label">l/h</p>
-                    <p className="form-input-column-text-label">{flowRatelh}</p>
+                    <p className="form-input-column-text-label">{bioreactorFormData.flowRatelh}</p>
                 </div>
                 
             </div>
 
-            <p className="form-separator">Titres</p>
+            {/* <p className="form-separator">Titres</p> */}
             <div className="form-input-cols">
-                <div className="form-input-column">
+                <div className="form-input-column-center">
                     <FormNumberInputSmall label="Titre mg/ml" />
                 </div>
 
                 <div className="form-input-column-center">
-                    <p className="form-input-column-text-label">mg/min</p>
+                    <p className="form-input-column-text-label">Titre mg/min</p>
                     <p className="form-input-column-text-label">{titremgmin}</p>
                 </div>
             </div>
             <p className="form-separator"></p>
             
-            <Button classes="btn btn-primary full-width-btn"> 🖩 Calculate</Button>
+            <Button classes="btn btn-primary full-width-btn"> 💾 Save Updated Data</Button>
         </form>
     );
 }
