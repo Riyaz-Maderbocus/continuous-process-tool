0d3aace09473e63cc44fba0550281aad2c468377
Riyaz Maderbocus <madsonray0@gmail.com>
2026-08-06 11:18:38 +0100

    make vi feed details

--- src/components/Forms/ViUpdateForm.jsx
+++ src/components/Forms/ViUpdateForm.jsx
@@ -35,7 +35,8 @@ const ViUpdateForm = ({unitOperation, closeModal, totalTime}) => {
             outputConc
         } = data
     
-    const [viFormData, setViFormData] = useState({                    
+    const [viFormData, setViFormData] = useState({
+            title,                    
             feedAverageFlowRate,
             feedFlowRateSetpoint,
             feedVolume,
@@ -65,14 +66,60 @@ const ViUpdateForm = ({unitOperation, closeModal, totalTime}) => {
 
     // Single form change like title
     const handleFormChange = (e) => {
-    setIldfFormData((prev) => ({
+    setViFormData((prev) => ({
         ...prev,
         [e.target.name]: e.target.value
     }))
     }
+
+    // Handle all UI changes
+    const handleAllChanges = (e) => {
+        const newfeedFlowRateSetpoint = e.target.name === "feedFlowRateSetpoint" ? parseFloat(e.target.value) : viFormData.feedFlowRateSetpoint;
+        const newFeedVolume = e.target.name === "feedVolume" ? parseFloat(e.target.value) : viFormData.feedVolume;
+        // Come back to this one later
+        // const newFeedAverageFlowRate = 
+        const newFeedTime = (newFeedVolume / newfeedFlowRateSetpoint).toFixed(3)
+
+        setViFormData((prev)=> ({
+            ...prev,
+            feedFlowRateSetpoint: newfeedFlowRateSetpoint,
+            feedVolume: newFeedVolume,
+            feedTime: newFeedTime,
+
+        }))
+    }
     return ( 
         <form className="form-container">
-            test
+            <FormTextInput label="Title" name="title" value={viFormData.title}
+            onChange={handleFormChange}
+            />
+            <p className="form-separator">Feed Details</p>
+            <div className="form-input-cols">
+
+                <div className="form-input-column-center">
+                    <p className="form-input-column-text-label">Average flow rate mL/min</p>
+                    <p className="form-input-column-text-output">{viFormData.feedAverageFlowRate}</p>
+                </div>
+
+                <div className="form-input-column-center">
+                    <FormNumberInputSmall label="Flow rate setpoint mL/min" name="feedFlowRateSetpoint"
+                    value={viFormData.feedFlowRateSetpoint}
+                    onChange={handleAllChanges}
+                    />
+                </div>
+
+                <div className="form-input-column-center">
+                    <FormNumberInputSmall label="Volume mL" name="feedVolume"
+                    value={viFormData.feedVolume}
+                    onChange={handleAllChanges}
+                    />
+                </div>
+                
+                <div className="form-input-column-center">
+                    <p className="form-input-column-text-label">Time min</p>
+                    <p className="form-input-column-text-output">{viFormData.feedTime}</p>
+                </div>
+            </div>
 
         </form>
      );
